369208d1ee33871f6a0d8058d3720f57ab8ac488
Euan <Euanmeston@gmail.com>
2022-03-13 21:59:54 +0000

    EOD commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="pySport80",
+    version="0.1.0",
+    description="pyAPI for Sport80",
+    url="https://github.com/euanwm/sport80_api",
+    author="Euan Meston",
+    author_email="euanmeston@gmail.com",
+    license="tbc",
+    install_requires=["requests",
+                      "beautifulsoup4"],
+    classifiers=["Programming Language :: Python :: 3.8",
+                 "Programming Language :: Python :: 3.9"]
+)
