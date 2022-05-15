61eda765caa26806b72136d93d971a949fff6331
Euan <Euanmeston@gmail.com>
2022-05-15 18:57:45 +0100

    V2.1.0

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 """ Setup file for PyPi """
 from setuptools import setup
 
+with open('README.md', 'r', encoding='utf-8') as readme_file:
+    README = readme_file.read()
+
 setup(
     name="sport80",
     version="2.1.0",
     description="Python API interface for the Sport80 sites",
+    long_description=README,
     url="https://github.com/euanwm/sport80_api",
     author="Euan Meston",
     author_email="euanmeston@gmail.com",
-    license="BSD 3",
+    license="BSD",
     install_requires=["requests",
                       "beautifulsoup4",
                       "js2py"],
