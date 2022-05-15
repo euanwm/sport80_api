260102b06abb16a7a04b1dd6d55dec44718e1ac1
Euan <Euanmeston@gmail.com>
2022-05-15 19:54:07 +0100

    V2.1.0

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
 """ Setup file for PyPi """
 from setuptools import setup
 
-with open('README.md', 'r', encoding='utf-8') as readme_file:
-    README = readme_file.read()
-
 setup(
     name="sport80",
     version="2.1.0",
     description="Python API interface for the Sport80 sites",
-    long_description=README,
+    long_description='Intentionally empty',
     url="https://github.com/euanwm/sport80_api",
     author="Euan Meston",
     author_email="euanmeston@gmail.com",
