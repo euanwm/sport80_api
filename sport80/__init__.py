45927471a29e91366205513ee28e7b72cde1ecdc
Euan <Euanmeston@gmail.com>
2022-05-15 22:55:09 +0100

    Type in version (init)

--- sport80/__init__.py
+++ sport80/__init__.py
@@ -7,5 +7,5 @@ from .sport80_http_client import SportEightyHTTP
 from .helpers import pull_tables, convert_to_json, convert_to_py
 from .pages_enum import EndPoint
 
-__version__ = "2.0.0"
+__version__ = "2.2.0"
 __author__ = "Euan Meston"
